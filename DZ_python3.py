b4d5643da56b481c3b5f2f0be38d79da970bf325
Polina <payline@mail.ru>
2022-05-05 10:04:04 +0300

    3

--- DZ_python3.py
+++ DZ_python3.py
@@ -1,24 +1,12 @@
 # Задайте последовательность чисел. 
 # Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
     
-# import random
-print ('Исходная последовательность чисел')
 numbers = [3, 5, 3, 8, 8, 2, 5]
+print ('Исходная последовательность чисел')
+print (numbers)
 new_numbers = []
-print (numbers)
-k = 0
-# while k <= numbers[len(numbers)-1]:
-# new_numbers.append(numbers[k])
-while k != len(numbers):
-    for i in range(len(numbers)):
-        if numbers[k] == numbers[i]:
-            # numbers.pop(i)
-            new_numbers.append(numbers[i])
-            # break
-            # print (k)
-            # print (i)
-            # print (new_numbers)
-    
-    k += 1
+for i in numbers:
+    if i not in new_numbers:
+        new_numbers.append(i)
 print ('Последовательность неповторяющихся элементов')
-print (numbers)
+print (new_numbers)
